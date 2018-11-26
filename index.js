d20ff6df93a8b69e161b588b98d3bc1b37226747
Boonyaritkae <45085813+Boonyaritkae@users.noreply.github.com>
2018-11-26 10:02:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+const HttpRequestModule = require('./modules/HttpRequestModule'),
+      MemchachedModule = require('./modules/MemcachedModule'),
+      ReserveDetailService = require('./services/reservedetail.service');
+
+exports.HttpRequestModule = HttpRequestModule;
+exports.MemchachedModule = MemchachedModule;
+exports.ReserveDetailService = ReserveDetailService;
+
---
+++ services/reservedetail.service.js
@@ -0,0 +1,27 @@
+var HttpRequestModule = require('../modules/HttpRequestModule');
+
+
+module.exports = class ReserveDetailModule extends HttpRequestModule {
+
+    constructor(reserveId = '') {
+      super();
+      this.reserveId = reserveId;
+      this.memcachedKey = `RESERVE_DETAIL[${this.reserveId}]`
+    }
+
+  async getReserveDetail() {
+   let resp = null;
+   try {
+    resp = await this.getDataMemcached(this.memcachedKey);
+    if (resp == null) {
+      resp = await this.createHttp(process.env.URL_RESERVE_DETAIL, {
+        performId: this.performId
+      }).post();
+      return resp.data.code == 100 ? resp.data.data : null;
+    }
+    return JSON.stringify(resp);
+  } catch (e) {
+    throw e;
+   }
+ }
+}
